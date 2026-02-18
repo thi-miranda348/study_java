21b9f39f6b5aad34ec9b2ced7757d5a923e861eb
Thi Anh Thi Nguyen <thi.miranda348@gmail.com>
2026-02-18 12:43:34 -0700

    Update RoomRepository

--- Console-Hotel-Manager/src/com/hotelapp/repository/RoomRepository.java
+++ Console-Hotel-Manager/src/com/hotelapp/repository/RoomRepository.java
@@ -3,33 +3,31 @@ package com.hotelapp.repository;
 import com.hotelapp.model.Room;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class RoomRepository {
-
-    private List<Room> rooms = new ArrayList<>();
-
+    private List<Room> rooms;
 
     public RoomRepository() {
-        rooms.add(new Room("101", "Standard", 100.0));
-        rooms.add(new Room("102", "Standard", 100.0));
-        rooms.add(new Room("201", "Deluxe", 200.0));
-        rooms.add(new Room("202", "Deluxe", 200.0));
+        this.rooms = new ArrayList<>();
+        rooms.add(new Room("101", "Standard", 150.0));
+        rooms.add(new Room("102", "Standard", 150.0));
+        rooms.add(new Room("201", "Deluxe", 250.0));
+        rooms.add(new Room("202", "Deluxe", 250.0));
         rooms.add(new Room("301", "Suite", 500.0));
-
-        System.out.println("-> [Repo] Database loaded with " + rooms.size() + " rooms.");
     }
+
 
     public List<Room> getAllRooms() {
         return new ArrayList<>(rooms);
     }
 
-    public Room getRoomById(String id) {
+    public Optional<Room> findRoomById(String roomId) {
         for (Room room : rooms) {
-            if (room.getRoomId().equalsIgnoreCase(id)) {
-                return room;
+            if (room.getRoomId().equalsIgnoreCase(roomId)) {
+                return Optional.of(room);
             }
         }
-        return null;
+        return Optional.empty();
     }
-
 }
---
+++ Console-Hotel-Manager/src/com/hotelapp/service/HotelService.java
@@ -0,0 +1,4 @@
+package com.hotelapp.service;
+
+public class HotelService {
+}
