8470be41941a968ba4bc62034fe26f62680fd9f5
Thi Anh Thi Nguyen <thi.miranda348@gmail.com>
2026-02-20 12:09:25 -0700

    Implement Account abstraction

---
+++ Console-Banking-System/src/com/bankingapp/model/Account.java
@@ -0,0 +1,35 @@
+package com.bankingapp.model;
+
+public abstract class Account {
+    private String accountNumber;
+    private String pin;
+    protected double balance;
+
+    public Account(String accountNumber, String pin, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public boolean validatePin(String enteredPin) {
+        return this.pin.equals(enteredPin);
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        }
+    }
+
+
+    public abstract void withdraw(double amount) throws Exception;
+}
