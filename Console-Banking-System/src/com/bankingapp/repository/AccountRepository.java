803000ddfbc077ba799bbeb22cebb8fd50a73b42
Thi Anh Thi Nguyen <thi.miranda348@gmail.com>
2026-02-20 12:12:18 -0700

    Add AccountRepository using HashMap for data simulation

---
+++ Console-Banking-System/src/com/bankingapp/repository/AccountRepository.java
@@ -0,0 +1,21 @@
+package com.bankingapp.repository;
+
+import com.bankingapp.model.Account;
+import com.bankingapp.model.SavingsAccount;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountRepository {
+    private Map<String, Account> database;
+
+    public AccountRepository() {
+        database = new HashMap<>();
+        database.put("1111", new SavingsAccount("1111", "1234", 500.0));
+        database.put("2222", new SavingsAccount("2222", "9999", 1000.0));
+    }
+
+    public Account findAccount(String accountNumber) {
+        return database.get(accountNumber);
+    }
+}
